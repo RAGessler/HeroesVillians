ca42aa12edb82a6f32a4e2b02a27ea8a1649ff0a
Robert Gessler <ragessler@Roberts-MacBook-Pro.local>
2022-04-15 16:47:10 -0500

    added custom response for get all supers

--- supers/urls.py
+++ supers/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.super_post),
+    path('', views.super_list),
     path('<int:pk>/', views.super_detail)
 ]
--- supers/views.py
+++ supers/views.py
@@ -2,17 +2,33 @@ from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+
+from super_types.models import SuperType
 from .serializers import SuperSerializer
 from .models import Super
 
 # Create your views here.
-@api_view(['POST'])
-def super_post(request):
-    serializer = SuperSerializer(data=request.data)
-    serializer.is_valid(raise_exception=True)
-    serializer.save()
-    return Response(serializer.data, status=status.HTTP_201_CREATED)
-    
+@api_view(['POST', 'GET'])
+def super_list(request):
+    if request.method == 'POST':
+        serializer = SuperSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    elif request.method == 'GET':
+        super_types = SuperType.objects.all()
+        custom_response_dictionary = {}
+        for super_type in super_types:
+
+            supers = Super.objects.filter(super_type_id=super_type.id)
+
+            super_serializer = SuperSerializer(supers, many=True)
+
+            custom_response_dictionary[super_type.type] = [
+            super_serializer.data
+            ]
+        return Response(custom_response_dictionary)
+
 @api_view(['GET', 'PUT', 'DELETE'])
 def super_detail(request, pk):
     super = get_object_or_404(Super, pk=pk)
